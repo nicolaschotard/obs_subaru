8da53f8e417f1ce2bcd0b552fb2a1d046aa06db4
Yusra AlSayyad <yusra@astro.princeton.edu>
2017-11-12 21:47:10 -0500

    Turn on CompareWarpAssemble as default algorithm in coaddDriver

--- config/coaddDriver.py
+++ config/coaddDriver.py
@@ -3,6 +3,12 @@ import os.path
 
 from lsst.utils import getPackageDir
 
+from lsst.pipe.tasks.assembleCoadd import CompareWarpAssembleCoaddTask
+config.assembleCoadd.retarget(CompareWarpAssembleCoaddTask)
+config.assembleCoadd.assembleStaticSkyModel.subregionSize = (10000, 200)
+config.assembleCoadd.assembleStaticSkyModel.badMaskPlanes = ("BAD", "EDGE", "SAT", "INTRP", "NO_DATA",)
+config.assembleCoadd.assembleStaticSkyModel.doApplyUberCal = True
+
 for sub in ("makeCoaddTempExp", "backgroundReference", "assembleCoadd", "processCoadd"):
     path = os.path.join(getPackageDir("obs_subaru"), "config", sub + ".py")
     if os.path.exists(path):
